7816e58b822dc0a44f31714d222505552e1fbf7d
NestorPala <npalavecino@fi.uba.ar>
2022-08-31 12:19:19 -0300

    little fixes

--- projects/Personal Page/scripts/index.js
+++ projects/Personal Page/scripts/index.js
@@ -1,22 +1,23 @@
-let skills = {
-    "HTML" : 4,
-    "CSS" : 3,
-    "JavaScript" : 3,
-    "Node.js" : 2,
-    "PHP" : 2,
-    "MySQL" : 3,
-    "Git" : 3,
-    "Python 3" : 3,
-    "C" : 3,
-    "TypeScript" : 3,
-    "Angular 2" : 2
-};
+let skills = [
+    "HTML",
+    "CSS",
+    "Bootstrap 5",
+    "JavaScript",
+    "Node.js",
+    "Express.js",
+    "MySQL",
+    "Git",
+    "Python 3",
+    "C",
+    "TypeScript",
+    "Angular 2"
+];
 
 
 function listSkills() {
     let skillList = document.getElementById("skills").getElementsByTagName("ul")[0];
 
-    for (let x in skills) {
+    for (let x of skills) {
         let li = document.createElement("li");
         li.setAttribute("style", "display: inline-block; margin: 20px;");
         li.innerHTML = x;

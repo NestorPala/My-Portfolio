caa6868fa2af0a232f58c4d566310f468f4382a3
NestorPala <npalavecino@fi.uba.ar>
2022-09-03 22:21:28 -0300

    added title writer effect to main page

---
+++ scripts/title.js
@@ -0,0 +1,22 @@
+const title = "My Web Portfolio";
+let titleMyPortfolio = document.getElementById("title-my-portfolio");
+let aestheticWriterCursor = document.getElementById("aesthetic-writer-cursor");
+const timeMs = 250;
+
+function showTitle(index) {
+    if (index == title.length) {
+        return;
+    }
+    setTimeout(() => {
+        if (index == 0) {
+            titleMyPortfolio.innerHTML = title[index];
+        } else {
+            titleMyPortfolio.innerHTML += title[index];
+        }
+        aestheticWriterCursor.innerHTML = aestheticWriterCursor.innerHTML == "" ? "_" : "";
+        showTitle(index + 1);
+    }, timeMs);
+}
+
+aestheticWriterCursor.innerHTML = "";
+showTitle(0);

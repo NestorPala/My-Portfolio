e9b7167e04b3dfc103555180abe731c018daa49b
NestorPala <npalavecino@fi.uba.ar>
2022-09-03 23:27:09 -0300

    Personal Page - added parallax effect

--- projects/Personal Page/scripts/index.js
+++ projects/Personal Page/scripts/index.js
@@ -13,34 +13,47 @@ let skills = [
     "Angular 2"
 ];
 
+let skillList = document.getElementById("skills").getElementsByTagName("ul")[0];
 
-function setGoToTopButton() {
-    let goToTopButton = document.getElementById("go-to-top");
-    goToTopButton.hidden = true;
-    window.onscroll = (e) => {
+for (let x of skills) {
+    let li = document.createElement("li");
+    li.setAttribute("style", "display: inline-block; margin: 20px;");
+    li.innerHTML = x;
+    skillList.appendChild(li);
+}
+
+let goToTopButton = document.getElementById("go-to-top");
+goToTopButton.hidden = true;
+
+goToTopButton.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+
+
+/*
+    Parallax effect extracted from:
+    https://stackoverflow.com/questions/29240028/css-make-a-background-image-scroll-slower-than-everything-else
+*/
+
+(function() {
+    let parallax = document.querySelectorAll("body");
+    let speed = 0.5;
+
+    window.onscroll = function() {
+      [].slice.call(parallax).forEach(function(element) {
+        let windowYOffset = window.pageYOffset;
+        let elBackgroundPos = "50% " + (windowYOffset * speed) + "px";
+
+        element.style.backgroundPosition = elBackgroundPos;
+
+        /////
+
         if (window.scrollY !== 0) {
             goToTopButton.hidden = false;
         } else {
             goToTopButton.hidden = true;
         }
+      });
     };
-    goToTopButton.addEventListener("click", () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-}
-
-
-function listSkills() {
-    let skillList = document.getElementById("skills").getElementsByTagName("ul")[0];
-
-    for (let x of skills) {
-        let li = document.createElement("li");
-        li.setAttribute("style", "display: inline-block; margin: 20px;");
-        li.innerHTML = x;
-        skillList.appendChild(li);
-    }
-}
-
-
-listSkills();
-setGoToTopButton();
+})();

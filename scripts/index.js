500344f099433424a0386fc5d15cdb1a32928771
NestorPala <npalavecino@fi.uba.ar>
2023-01-04 15:22:25 -0300

    refactor project-link modal code

--- scripts/index.js
+++ scripts/index.js
@@ -38,28 +38,29 @@ Array.prototype.forEach.call(projectLinks, element => {
     let modal = document.createElement("div");
 
     modal.innerHTML = `    
-                        <!-- Modal -->
-                        <div class="modal fade" id="` + element.id + `Modal" tabindex="-1" aria-labelledby="` + element.id + `ModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                <h5 class="modal-title" id="` + element.id + `ModalLabel">Open this project</h5>
-                                <button type="button" id="modal-close-button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                Do you want to open the project or to review the code in its repository?
-                                </div>
-                                <div class="modal-footer">
-                                <button type="button" id="open-project-button" class="btn">
-                                <a href="` + element.href + `">Show me the app!</a>
-                                </button>
-                                <button type="button" id="visit-repo-button" class="btn">
-                                <a href="` + repoLinks[element.id] + `">Visit Repo</a>
-                                </button>
-                                </div>
-                            </div>
-                            </div>
-                        </div>`;
+        <!-- Modal -->
+        <div class="modal fade" id="${element.id}Modal" tabindex="-1" aria-labelledby="${element.id}ModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                <h5 class="modal-title" id="${element.id}ModalLabel">Open this project</h5>
+                <button type="button" id="modal-close-button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                Do you want to open the project or to review the code in its repository?
+                </div>
+                <div class="modal-footer">
+                <button type="button" id="open-project-button" class="btn">
+                <a href="${element.href}">Show me the app!</a>
+                </button>
+                <button type="button" id="visit-repo-button" class="btn">
+                <a href="${repoLinks[element.id]}">Visit Repo</a>
+                </button>
+                </div>
+            </div>
+            </div>
+        </div>`
+    ;
 
     document.body.appendChild(modal);
 });

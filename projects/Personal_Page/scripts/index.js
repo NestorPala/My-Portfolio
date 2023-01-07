b95aa508ddd4113e6070485dfeaa1c5722fcc3c8
NestorPala <npalavecino@fi.uba.ar>
2023-01-07 00:38:35 -0300

    personal page: disabled anchor tags on section buttons

--- projects/Personal_Page/scripts/index.js
+++ projects/Personal_Page/scripts/index.js
@@ -64,3 +64,38 @@ window.onload = function () {
         a[i].setAttribute('target', '_blank');
     }
 };
+
+
+const pageSections = [
+    {
+        sectionButtonId: "section-education-button", 
+        sectionElementId: "education"
+    },
+    {
+        sectionButtonId: "section-experience-button", 
+        sectionElementId: "work-experience"
+    },
+    {
+        sectionButtonId: "section-skills-button", 
+        sectionElementId: "skills"
+    },
+    {
+        sectionButtonId: "section-hobbies-button", 
+        sectionElementId: "hobbies"
+    },
+    {
+        sectionButtonId: "section-resume-button", 
+        sectionElementId: "resume"
+    },
+];
+
+pageSections.forEach(function(section) {
+    const sectionButton = document.getElementById(section["sectionButtonId"]);
+    const sectionElement = document.getElementById(section["sectionElementId"]);
+
+    sectionButton.addEventListener("click", function(event) {
+        sectionElement.scrollIntoView({
+            behavior: "smooth"
+        });
+    })
+});

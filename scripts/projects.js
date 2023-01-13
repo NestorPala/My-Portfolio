a1e2baf2138e52df56edc5ceccdb2a793e6e27d4
NestorPala <npalavecino@fi.uba.ar>
2023-01-13 16:11:21 -0300

    main page: added flag 'deployed' to projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -3,31 +3,36 @@ const projects = [
         id: "personal-page",
         href: "projects/Personal_Page/",
         buttonText: "About me",
-        repoLink: "https://github.com/NestorPala/Web-Projects/tree/main/projects/Personal_Page"
+        repoLink: "https://github.com/NestorPala/Web-Projects/tree/main/projects/Personal_Page",
+        deployed: true
     },
     {
         id: "todo-list",
         href: "projects/To-Do_List/",
         buttonText: "To-Do List",
-        repoLink: "https://github.com/NestorPala/To-Do-List"
+        repoLink: "https://github.com/NestorPala/To-Do-List",
+        deployed: true
     },
     {
         id: "typing-practice",
         href: "projects/Typing_Practice/",
         buttonText: "Typing Practice",
-        repoLink: "https://github.com/NestorPala/Web-Projects/tree/main/projects/Typing_Practice"
+        repoLink: "https://github.com/NestorPala/Web-Projects/tree/main/projects/Typing_Practice",
+        deployed: true
     },
     {
         id: "product-list",
         href: "https://products-app-7ssx.onrender.com",
         buttonText: "Product List",
-        repoLink: "https://github.com/NestorPala/Products-App" 
+        repoLink: "https://github.com/NestorPala/Products-App",
+        deployed: true 
     },
     {
         id: "notes-app",
         href: "https://notes-app-81r1.onrender.com",
         buttonText: "Notes App (React)",
-        repoLink: "https://github.com/NestorPala/Notes-App" 
+        repoLink: "https://github.com/NestorPala/Notes-App",
+        deployed: true 
     },
 ];
 
@@ -35,13 +40,16 @@ const repoLinks = {};
 projects.map(project => repoLinks[project["id"]] = project["repoLink"]);
     
 function addProjects() {
+    const noDeployPage = "messages/no-deploy.html";
+    const isDeployed = project => project["deployed"] === true;
+
     for (const project of projects) {
         const projectList = document.getElementById("project-list");
         const newProjectElement = document.createElement("a");
     
         newProjectElement.className = "project-link";
         newProjectElement.id = project["id"];
-        newProjectElement.href = project["href"];
+        newProjectElement.href = isDeployed(project) ? project["href"] : noDeployPage;
         newProjectElement.dataset.bsToggle = "modal";
         newProjectElement.dataset.bsTarget = `#${project["id"]}Modal`;
         newProjectElement.innerText = project["buttonText"];

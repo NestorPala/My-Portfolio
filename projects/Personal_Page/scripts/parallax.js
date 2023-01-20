b889884ded6de466b71e99364804de991328e59e
NestorPala <npalavecino@fi.uba.ar>
2023-01-20 12:10:35 -0300

    modified parallax to remove lag

--- projects/Personal_Page/scripts/parallax.js
+++ projects/Personal_Page/scripts/parallax.js
@@ -4,13 +4,39 @@
 */
 
 const backgroundSpeed = 0.2; //from 0.0 to 1.0
-const bodyElements = document.querySelectorAll("body");
+const background = document.getElementById("background");
 
 function parallax() {
-    Array.prototype.forEach.call(bodyElements, function(element) {
-        const windowYOffset = window.pageYOffset;
-        const backgroundPosition = "50% " + (windowYOffset * (1 - backgroundSpeed)) + "px";
+    let widthVH = 37.5;
+    let heightVW = 40;
 
-        element.style.backgroundPosition = backgroundPosition;
-    });
+
+    const documentWidth = window.innerWidth;
+    const documentHeight = document.body.scrollHeight;
+    const windowYOffset = window.pageYOffset;
+
+    let speedFactor, widthFactor ,heightFactor;
+    if (documentWidth < 500) {
+        speedFactor = 0.43;
+        widthFactor = 1.0;
+        heightFactor = 5.9;    
+    } else if (documentWidth < 1000) {
+        speedFactor = 1;
+        widthFactor = 1.3;
+        heightFactor = 5;  
+    } else {
+        speedFactor = 0.6;
+        widthFactor = 1.3; 
+        heightFactor = 5;   
+    }
+    
+    const newSpeed = backgroundSpeed / (1 / speedFactor);
+    const newTop = windowYOffset * newSpeed;
+    const newWidth = widthFactor * newSpeed * (widthVH * (windowYOffset / documentWidth));
+    const newHeight = heightFactor * newSpeed * (heightVW * (windowYOffset / documentHeight));
+
+    background.style.top = `-${newTop}px`;
+    background.style.width = `calc(150% + ${widthVH + newWidth}vh)`;
+    background.style.height = `calc(150% + ${heightVW + newHeight}vw)`;
+
 }

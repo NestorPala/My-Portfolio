ebb5c323cf223fe124d7061b01a84d53e295a8a0
NestorPala <npalavecino@fi.uba.ar>
2023-01-23 23:21:41 -0300

    main page: minifix dropdown opacity effect

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -8,12 +8,15 @@ const dropDownButton = document.getElementById("dropdownMenuButton1");
 const options = { attributes: true };
 
 function callback(mutationList, observer) {
+    const mainTitle = document.getElementById("main-title-content");
     mutationList.forEach(function(mutation) {
         if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
             if (Array.from(dropDownButton.classList).includes("show")) {
                 bodyElementsExceptDropdown.forEach(e => e.style.opacity = 0.3);
+                mainTitle.style.color = "#735383";
             } else {
                 bodyElementsExceptDropdown.forEach(e => e.style.opacity = 1.0);
+                mainTitle.style.color = "white";
             }
         }
     });

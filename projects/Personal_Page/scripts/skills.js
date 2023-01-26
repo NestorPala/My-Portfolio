4fba2c973214380e1060d009b4a3a005cb96e1b0
NestorPala <npalavecino@fi.uba.ar>
2023-01-26 20:25:52 -0300

    Personal Page -> About Me: added updated information and some styling

--- projects/Personal_Page/scripts/skills.js
+++ projects/Personal_Page/scripts/skills.js
@@ -1,23 +1,27 @@
 const skills = [
     "HTML",
     "CSS",
-    "Bootstrap 5",
+    "Bootstrap",
     "JavaScript",
     "Node.js",
     "Express.js",
+    "MongoDB",
+    "PHP",
+    "Laravel",
     "MySQL",
     "Git",
     "Python 3",
     "C",
     "TypeScript",
-    "Angular 2"
+    "React",
+    "Angular 2+",
 ];
 
 const skillList = document.getElementById("skills").getElementsByTagName("ul")[0];
 
 for (const skill of skills) {
     const li = document.createElement("li");
-    li.setAttribute("style", "display: inline-block; margin: 20px;");
     li.innerHTML = skill;
+    li.className = "tech-skill-item";
     skillList.appendChild(li);
 }
